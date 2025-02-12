5b24cba619cf95d66ff25f1577501eda145a5ce7
Andini Putriansyah <andinpansyah@gmail.com>
2025-02-12 14:27:26 +0700

    komversi ver 2

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ document.getElementById("form-convertion").addEventListener("submit", function (
         let fahrenheit = (celcius * 9/5) + 32;
         console.log(celcius + '°C sudah dikonversi menjadi ' + fahrenheit + '°F');
         document.getElementById("input-fahrenheit").value = fahrenheit.toFixed(1);
-        document.getElementById("result-calculation").value = celcius + '°C' + ' × ' + '9/5 + 32 = ' + fahrenheit.toFixed(1) + '°F'; 
+        document.getElementById("result-calculation").value = '(' + celcius + '°C' + ' × ' + '9/5) + 32 = ' + fahrenheit.toFixed(1) + '°F'; 
     }     else {
         document.getElementById("input-fahrenheit").value = "";
     }
@@ -20,8 +20,9 @@ document.getElementById("form-convertion").addEventListener("submit", function (
         let celcius = (fahrenheit - 32) / (9/5);
         console.log(fahrenheit + '°C sudah dikonversi menjadi ' + celcius.toFixed(1) + '°F');
         document.getElementById("input-celcius").value = celcius.toFixed(1);
-        document.getElementById("result-calculation").value = fahrenheit + '°F' + ' - 32 ' + '/ (9/5) = ' + celcius.toFixed(1) + '°C'; 
+        document.getElementById("result-calculation").value = '(' + fahrenheit + '°F' + ' - 32) ' + '/ (9/5) = ' + celcius.toFixed(1) + '°C'; 
     }     else {
         document.getElementById("input-fahrenheit").value = "";
     }
 }
+
